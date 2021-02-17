1b9e737640cdca9e446ed04851c74bf982c2b069
Yves Loeys <yves.loeys@gmail.com>
2021-02-17 10:36:47 -0800

    placeholder text when no favs

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -4,10 +4,19 @@ import MealList from "../components/MealList";
 import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
+import DefaultText from "../components/DefaultText";
 
 const FavoritesScreen = (props) => {
   //getting the state from the redux store
   const favMeals = useSelector((state) => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No Favorites yet! </DefaultText>
+      </View>
+    );
+  }
 
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
@@ -28,5 +37,13 @@ FavoritesScreen.navigationOptions = (navData) => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default FavoritesScreen;
